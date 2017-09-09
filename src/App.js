3d96d14e47a75af73a0d52f14ab849b1a0291be7
Kevin Ndung'u <kevgathuku@gmail.com>
2017-09-09 12:26:27 +0300

    Add correct events for stage mouse listeners

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from "react";
-import { Layer, Stage, Line } from "react-konva";
+import { Layer, Stage } from "react-konva";
 import KonvaLine from "./KonvaLine";
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.initialFigure = {
-            id: Math.random(),
+            id: `${Math.random()}-${Math.random()}`,
             x: 0,
             x1: 0,
             y: 0,
             y1: 0
         };
+
+        this.drawnFigures = [
+            {
+                id: `${Math.random()}-${Math.random()}`,
+                x: 10,
+                x1: 50,
+                y: 10,
+                y1: 50,
+                type: "line"
+            }
+        ];
 
         this.state = {
             listenForMouseMove: false,
@@ -19,8 +30,12 @@ class App extends Component {
         };
     }
 
+    addFigure = figure => {
+        if (!figure.id) figure.id = `${Math.random()}-${Math.random()}`;
+        this.drawnFigures.push(figure);
+    };
+
     onMouseDown = evt => {
-        debugger;
         console.log("onMouseDown evt", evt);
         const { evt: { offsetX, offsetY } } = evt;
 
@@ -58,6 +73,7 @@ class App extends Component {
         const { listenForMouseMove } = this.state;
 
         if (listenForMouseMove) {
+            this.addFigure(this.state.figure);
             this.setState(prevState => ({
                 listenForMouseMove: false,
                 figure: null
@@ -67,8 +83,6 @@ class App extends Component {
 
     renderFigureOnCanvas = figure => {
         if (!figure || !figure.type) return null;
-
-        console.log("Figure", figure);
 
         switch (figure.type) {
             case "line":
@@ -85,21 +99,13 @@ class App extends Component {
                 <Stage
                     width={600}
                     height={600}
-                    onMouseDown={this.onMouseDown}
-                    onMouseMove={this.onMouseMove}
-                    onMouseUp={this.onMouseUp}
+                    onContentMousedown={this.onMouseDown}
+                    onContentMouseMove={this.onMouseMove}
+                    onContentMouseUp={this.onMouseUp}
                     listening
                 >
                     <Layer listening>
-                        {this.state.figure.type ? (
-                            this.renderFigureOnCanvas(this.state.figure)
-                        ) : null}
-                        <Line
-                            points={[10, 10, 50, 50]}
-                            stroke="blue"
-                            strokeWidth={5}
-                            lineCap="round"
-                        />
+                        {this.drawnFigures.map(this.renderFigureOnCanvas)}
                     </Layer>
                 </Stage>
             </div>
